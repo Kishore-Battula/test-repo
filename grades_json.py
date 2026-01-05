965df8629db0a8c6a7c0647d24a25a00d43d02a1
Kishore B <battula.kishore@gmail.com>
2026-01-04 23:45:33 -0500

    Add scripts for reading and writing JSON files
    Introduced ReadJason.py to read and pretty-print a JSON file, WriteJsonFile.py to write account data to a JSON file, and grades_json.py to write grade data and read account data from JSON files. These scripts demonstrate basic JSON file operations for account and grade management.

---
+++ grades_json.py
@@ -0,0 +1,19 @@
+import json
+
+grades_dict={
+    'gradebook':
+        [
+            {'student_id':1,'name':'Red','grade':'A'},
+            {'student_id':2,'name':"Yellow",'grade':'A'},
+            {'student_id':3,'name':'White','grade':'C'}
+        ]
+}
+
+with open('/Users/kishorebattula/Desktop/Kishore/grades_jason.txt','a') as records:
+       json.dump(grades_dict,records)
+       print('Grades.txt file was succesfully created.')
+
+with open('/Users/kishorebattula/Desktop/Kishore/accounts_jason.txt','r') as records:
+       print(json.dumps(json.load(records),indent=4))
+
+
